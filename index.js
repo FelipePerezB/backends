2f9454bbecdbee7a2dc421c70eb51504efad93dd
= <=>
2023-01-13 15:34:15 -0300

    v3

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 require('./utils/auth');
 const express = require('express');
+const cors = require('cors');
 const { port } = require('./config');
 const routerApi = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 routerApi(app);
 
